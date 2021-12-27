4df2eae6f0225ca898593614a145eeaeaba502ac
dharmpadaliya98 <padliyadharm98@gmail.com>
2021-12-27 18:23:28 +0530

    object rendering from prop

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
 
 const Hello = (props) => {
+  let z = props.course
+  console.log(z?.a)
   return (
     <div>
-      <p>Hello {props.name} {props.age}</p>
+      <p> {props.name} {props.age}</p>
+      <p> Object rendering : {z?.abc.toString()} </p>
     </div>
   )
+
 }
+
 
 function App() {
   console.log('Hello from component')
   const now = new Date()
   const a = 10
   const b = 20
-
+  let z = {
+    a: 'a',
+    abc: ['abcdefg', 'hijklmn']
+  }
   return (
     <div>
       <header>
-        <Hello name="Dp" age='24' />
-        <p> Hello New user {now.toString()}</p>
+        <Hello name="Dp" age='24' course={z} />
+        <p> Its {now.toString()}</p>
         <p> {a} + {b} is {a + b} </p>
       </header>
+
     </div>
   );
 }
