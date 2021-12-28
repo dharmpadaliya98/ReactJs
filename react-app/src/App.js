859aee067f5a5fe2c2dc85badd866f32b39b5918
dharmpadaliya98 <padliyadharm98@gmail.com>
2021-12-28 11:24:26 +0530

    Stateful component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,50 +1,58 @@
 import './App.css';
+import React, { useState } from 'react'
 
-const Hello = (props) => {
-  let z = props.arto
-  console.log(z)
-  const bornYear = () => new Date().getFullYear() - props.age
-  return (
-    <div>
-      <p> {props.name} {props.age}</p>
-      {/* <p> Object rendering : {z?.abc.toString()} </p> */}
-      <p>{z.greet()}</p>
-      <p>So you were probably born in {bornYear()}</p>
+// const Hello = (props) => {
+//   let z = props.arto
+//   const { name, age } = props
+//   console.log(z)
+//   const bornYear = () => new Date().getFullYear() - age
+//   return (
+//     <div>
+//       <p> {props.name} {props.age}</p>
+//       {/* <p> Object rendering : {z?.abc.toString()} </p> */}
+//       <p>{z.greet()}</p>
+//       <p>So you were probably born in {bornYear()}</p>
 
-    </div>
+//     </div>
+//   )
+// }
+
+
+
+function App(props) {
+  // console.log('Hello from component')
+  // const now = new Date()
+  // const a = 10
+  // const b = 20
+  // let z = {
+  //   a: 'a',
+  //   abc: ['abcdefg', 'hijklmn']
+  // }
+  // const arto = {
+  //   name: 'Arto Hellas',
+  //   age: 35,
+  //   education: 'PhD',
+  //   greet: function () {
+  //     console.log('hello, my name is ' + this.name + 'my age is ' + this.age)
+  //   },
+
+  // }
+
+  const [counter, setCounter] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    5000
   )
-}
-
-
-
-function App() {
-  console.log('Hello from component')
-  const now = new Date()
-  const a = 10
-  const b = 20
-  let z = {
-    a: 'a',
-    abc: ['abcdefg', 'hijklmn']
-  }
-  const arto = {
-    name: 'Arto Hellas',
-    age: 35,
-    education: 'PhD',
-    greet: function () {
-      console.log('hello, my name is ' + this.name + 'my age is ' + this.age)
-    },
-
-  }
-
 
   return (
     <div>
-      <header>
+      {/* <header>
         <Hello name="Dp" age='24' arto={arto} />
         <p> Its {now.toString()}</p>
         <p> {a} + {b} is {a + b} </p>
-      </header>
-
+      </header> */}
+      {counter}
     </div>
   );
 
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+let counter = 1
+
 ReactDOM.render(
-  <App />,
+  <App counter={counter}/>,
   document.getElementById('root')
 );
+
+
 
 // reportWebVitals();
