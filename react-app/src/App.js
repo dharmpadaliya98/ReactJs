46b9d2785fa6ef6aacf7c054f8b55b672b506705
dharmpadaliya98 <padliyadharm98@gmail.com>
2021-12-28 10:48:05 +0530

    Component helper functions

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 
 const Hello = (props) => {
-  let z = props.course
-  console.log(z?.a)
+  let z = props.arto
+  console.log(z)
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
   return (
     <div>
       <p> {props.name} {props.age}</p>
-      <p> Object rendering : {z?.abc.toString()} </p>
+      {/* <p> Object rendering : {z?.abc.toString()} </p> */}
+      <p>{z.greet()}</p>
+      <p>So you were probably born in {bornYear()}</p>
+
     </div>
   )
 
 }
+
 
 
 function App() {
@@ -22,16 +30,31 @@ function App() {
     a: 'a',
     abc: ['abcdefg', 'hijklmn']
   }
+  const arto = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+    greet: function () {
+      console.log('hello, my name is ' + this.name + 'my age is ' + this.age)
+    },
+
+  }
+
+
+
+
   return (
     <div>
       <header>
-        <Hello name="Dp" age='24' course={z} />
+        <Hello name="Dp" age='24' arto={arto} />
         <p> Its {now.toString()}</p>
         <p> {a} + {b} is {a + b} </p>
       </header>
 
     </div>
   );
+
+
 }
 
 export default App;
